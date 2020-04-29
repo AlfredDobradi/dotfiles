e47d9ec8a231e758448b113f09783c031650b682
Alfred Dobradi <alfreddobradi@gmail.com>
2020-04-29 21:50:22 +0100

    Helper to copy new files

---
+++ refresh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p ./config
+mkdir -p ./Pictures
+
+cp -r ~/.config/i3 ./.config/
+cp -r ~/.config/rofi ./.config/
+cp -r ~/.config/nvim ./.config
+cp ~/.zshrc ./
+
+cp ~/Pictures/anarchism_w.png ./Pictures/anarchism_w.png
