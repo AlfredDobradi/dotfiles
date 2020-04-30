483807ed6de9bf55577eb2ed43a0c268779ff956
Alfred Dobradi <alfreddobradi@gmail.com>
2020-04-30 11:44:47 +0100

    Taskwarrior and add it to polybar

---
+++ .config/polybar/scripts/task_polybar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+most_urgent_desc=`task rc.verbose: rc.report.next.columns:description rc.report.next.labels:1 limit:1 next`
+most_urgent_id=`task rc.verbose: rc.report.next.columns:id rc.report.next.labels:1 limit:1 next`
+most_urgent_due=`task rc.verbose: rc.report.next.columns:due.relative rc.report.next.labels:1 limit:1 next`
+echo "$most_urgent_id" > /tmp/tw_polybar_id
+
+output="$most_urgent_desc"
+if [ -n "$most_urgent_due" ];
+then output="$most_urgent_desc  $most_urgent_due";
+fi
+
+echo "$output" 
--- refresh.sh
+++ refresh.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 mkdir -p ./.config
 mkdir -p ./Pictures
+mkdir -p ./.task
 
 cp -r ~/.config/i3 ./.config/
 cp -r ~/.config/rofi ./.config/
 cp -r ~/.config/nvim ./.config
 cp -r ~/.config/polybar ./.config
 cp ~/.taskrc ./
+cp ~/.task/brvy.theme ./.task
 cp ~/.zshrc ./
 
 cp ~/Pictures/anarchism_w.png ./Pictures/anarchism_w.png
