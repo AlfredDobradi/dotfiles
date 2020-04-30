929e50efbada6be272ac82eb1570d47aef4864ee
Alfred Dobradi <alfreddobradi@gmail.com>
2020-04-30 09:33:32 +0100

    Polybar config

--- refresh.sh
+++ refresh.sh
@@ -5,6 +5,7 @@ mkdir -p ./Pictures
 cp -r ~/.config/i3 ./.config/
 cp -r ~/.config/rofi ./.config/
 cp -r ~/.config/nvim ./.config
+cp -r ~/.config/polybar ./.config
 cp ~/.zshrc ./
 
 cp ~/Pictures/anarchism_w.png ./Pictures/anarchism_w.png
